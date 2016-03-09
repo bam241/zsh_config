9928a303fa39bf06e2d467794f697c2c1a2e1830
Mouginot Baptiste <Baaaaam@users.noreply.github.com>
2016-03-09 17:57:03 -0600

    Update install.sh
    add some cd to avoid strange behavior :)

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+cd
 git clone git@github.com:Baaaaam/zsh_config.git .zsh_config
 cd .zsh_config
 git clone git@github.com:Baaaaam/oh-my-zsh.git oh-my-zsh
+cd
 mv .zshrc .zshrc_bak
 ln -s .zsh_config/zshrc .zshrc
