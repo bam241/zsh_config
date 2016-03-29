1ea0c5ea2993337cf5ce3393010686d1a1b863f4
Line SOURIS <lineminimoon@d128104184078.engr.wisc.edu>
2016-03-29 13:24:29 -0500

    update install

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ git clone git@github.com:Baaaaam/oh-my-zsh.git oh-my-zsh
 mv .zshrc .zshrc_bak
 ln -s .zsh_config/zshrc .zshrc
 ln -s .zsh_config/gitconfig .gitconfig
+cd .zsh_config/oh-my-zsh/plugins
+git clone git@github.com:Baaaaam/zsh-syntax-highlighting.git 
