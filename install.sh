5d06ab64f50f75ca02477431de8503899b0cacc3
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-11-23 11:16:19 -0600

    update install

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
+git fetch --all
+git pull
 # Getting Omzsh & fonts for iterm
 git clone https://github.com/Baaaaam/oh-my-zsh.git oh-my-zsh
 git clone https://github.com/powerline/fonts.git
 
 git clone http://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/plugins/zsh-syntax-highlighting
 
-# Backing-up the existing file
-mv ../.zshrc ../.zshrc_bak
-mv ../.gitconfig ../.gitconfig_bak
-mv ../.vimrc .vimrc_bak
-mv ../.vim .../vim_bak
 
 # Installing the new config
 ln -s ../.zsh_config/zshrc ../.zshrc
 ln -s ../.zsh_config/gitconfig ../.gitconfig
 ln -s ../.zsh_config/vim ../.vim
 ln -s ../.zsh_config/vimrc ../.vimrc
-ln -s ../.zsh_config/git-change-url ../.local/bin/
