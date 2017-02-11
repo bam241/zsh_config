d1942f85d4b5df61f22b6158fc3650992020ca08
MOUGINOT B <mouginot.baptiste@gmail.com>
2017-02-11 14:02:25 -0600

    change names

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Getting Omzsh & fonts for iterm
-git clone https://github.com/bam241/oh-my-zsh.git oh-my-zsh
+git clone https://github.com/bam241/oh-my-zsh.git zsh/oh-my-zsh
 git clone https://github.com/powerline/fonts.git
 
-git clone http://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone http://github.com/zsh-users/zsh-syntax-highlighting.git zsh/oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone http://github.com/rhysd/vim-clang-format.git vim/vim-clang-format
 
 
@@ -15,8 +15,8 @@ mv ~/.vimrc ~/.vimrc_bak
 mv ~/.vim ~./vim_bak
 
 # Installing the new config
-ln -s ~/.zsh_config/zshrc ~/.zshrc
-ln -s ~/.zsh_config/gitconfig ~/.gitconfig
-ln -s ~/.zsh_config/vim ~/.vim
-ln -s ~/.zsh_config/vimrc ~/.vimrc
+ln -s ~/.zsh_config/zsh/zshrc ~/.zshrc
+ln -s ~/.zsh_config/git/gitconfig ~/.gitconfig
+ln -s ~/.zsh_config/vim/vim ~/.vim
+ln -s ~/.zsh_config/vim/vimrc ~/.vimrc
 ln -s ~/.zsh_config/macro/git-change-url ~/.local/bin/
