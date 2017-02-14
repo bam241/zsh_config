cb937c4a958fe5d0d30dee034e76ca35ec54573a
Mouginot B <mouginot.baptiste@gmail.com>
2017-02-14 17:02:32 -0600

    update

--- install.sh
+++ install.sh
@@ -2,15 +2,12 @@
 
 # Getting Omzsh & fonts for iterm
 git submodule update --init --recursive
-git clone http://github.com/zsh-users/zsh-syntax-highlighting.git zsh/oh-my-zsh/plugins/zsh-syntax-highlighting
-
-
 
 # Backing-up the existing file
 mv ~/.zshrc ~/.zshrc_bak
 mv ~/.gitconfig ~/.gitconfig_bak
 mv ~/.vimrc ~/.vimrc_bak
-mv ~/.vim ~./vim_bak
+mv ~/.vim ~/.vim_bak
 
 # Installing the new config
 ln -s ~/.zsh_config/zsh/zshrc ~/.zshrc
