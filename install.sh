0d6db0b59bd17475e3db771cb491c747ca6ea0c5
Mouginot B <mouginot.baptiste@gmail.com>
2017-03-01 13:52:26 -0600

    update install add tmux + powerline config

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 rm_or_mv() {
-    if [[ -L ${1} ]]
-    then
-        rm ${1}
-    else
+    if [[ -L ${1} ]]; then
+        rm -rf ${1}
+    elif [[ -e $1} ]]; then
         mv ${1} ${1}_bak
     fi
 }
@@ -13,22 +12,28 @@ rm_or_mv() {
 # Getting Omzsh & fonts for iterm
 git submodule update --init --recursive
 
-# Backing-up the existing file
-
 rm_or_mv ${HOME}/.zshrc
-rm_or_mv ${HOME}/.gitconfig
-rm_or_mv ${HOME}/.vimrc
-rm_or_mv ${HOME}/.vim
-rm_or_mv ${HOME}/.tmux.conf
-rm_or_mv ${HOME}/.rootlogon.C
-rm_or_mv ${HOME}/.clang-format
-
-
-# Installing the new config
 ln -s ${HOME}/.zsh_config/zsh/zshrc ${HOME}/.zshrc
-ln -s ${HOME}/.zsh_config/git/gitconfig ${HOME}/.gitconfig
-ln -s ${HOME}/.zsh_config/vim/vim ${HOME}/.vim
-ln -s ${HOME}/.zsh_config/vim/vimrc ${HOME}/.vimrc
+
+rm_or_mv ${HOME}/.tmux.conf
 ln -s ${HOME}/.zsh_config/tmux/tmux.conf ${HOME}/.tmux.conf
-ln -s ${HOME}/.zsh_config/root/rootlogon.C ${HOME}/.rootlogon.C
+
+rm_or_mv ${HOME}/.config/powerline
+ln -s ${HOME}/.zsh_config/powerline ${HOME}/.config/powerline
+
+rm_or_mv ${HOME}/.gitconfig
+ln -s ${HOME}/.zsh_config/git/gitconfig ${HOME}/.gitconfig
+
+
+rm_or_mv ${HOME}/.vim
+rm_or_mv ${HOME}/.vimrc
+ln -s "${HOME}/.zsh_config/vim/vim" "${HOME}/.vim"
+ln -s "${HOME}/.zsh_config/vim/vimrc" "${HOME}/.vimrc"
+
+rm_or_mv ${HOME}/.clang-format
 ln -s ${HOME}/.zsh_config/clang-format/clang-format ${HOME}/.clang-format
+
+
+rm_or_mv ${HOME}/.rootlogon.C
+ln -s ${HOME}/.zsh_config/root/rootlogon.C ${HOME}/.rootlogon.C
+
